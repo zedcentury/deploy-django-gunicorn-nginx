2c647e745d9e200a6362412369c2492fb4e149cc
asliddin750750 <asliddin750750@gmail.com>
2023-08-30 20:40:44 +0500

    database.sh

--- database.sh
+++ database.sh
@@ -9,10 +9,13 @@ db_user=$(jq '.db.user' "$json_file")
 db_password=$(jq -r '.db.password' "$json_file")
 
 # Create database and set owner to database
-sudo -u postgres psql <<ENDPSQL
-    CREATE DATABASE "$db_name";
-    CREATE USER "$db_user" WITH PASSWORD "$db_password";
-    GRANT ALL PRIVILEGES ON DATABASE "$db_name" TO "$db_user";
-    ALTER DATABASE "$db_name" OWNER TO "$db_user";
-    \q
-ENDPSQL
+# Run psql command to create a database
+sudo -u postgres psql -c "CREATE DATABASE $db_name WITH OWNER = $db_user;"
+
+#sudo -u postgres psql <<ENDPSQL
+#    CREATE DATABASE "$db_name";
+#    CREATE USER "$db_user" WITH PASSWORD "$db_password";
+#    GRANT ALL PRIVILEGES ON DATABASE "$db_name" TO "$db_user";
+#    ALTER DATABASE "$db_name" OWNER TO "$db_user";
+#    \q
+#ENDPSQL
