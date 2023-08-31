6d3abe5016ddfa4581c139d31bfac3040128e553
asliddin750750 <asliddin750750@gmail.com>
2023-08-31 19:23:23 +0500

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt update
-apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx
+apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib nginx jq
